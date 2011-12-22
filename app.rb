d0a0fbb821ffec7bc4410eca783db87a8a2eb8aa
yayugu <yayugu@gmail.com>
2011-12-23 08:47:20 +0900

    fix

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ memo = {}
 get '/:id/:size' do |id, size|
   begin
     if memo[id]
-      n = memo[id]
+      n, ext = memo[id]
     else
       n = Nokogiri::HTML(open("http://twitter.com/#{id}").read)
       n = n.css('img').first
       n = n['src'].to_s
       puts n
       ext = n.match(/(jpg|png|gif|jpeg)$/)
-      n = n.match(%r|http\://a[0-9]\.twimg\.com/profile_images/[0-9]+/[0-9\_(n\_)]+|)
-      memo[id] = n
+      n = n.gsub(%r|(^.*)reasonably_small\....$|, '\1')
+      memo[id] = [n, ext]
     end
     n = "#{n}#{size}.#{ext}"
     puts n
